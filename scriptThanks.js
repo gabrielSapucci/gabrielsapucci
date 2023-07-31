e6f0ae0cf7cc8ce6067af2f07fd2363f057738a8
gabrielSapucci <110752748+gabrielSapucci@users.noreply.github.com>
2023-07-31 16:16:53 -0300

    Update scriptThanks.js

--- scriptThanks.js
+++ scriptThanks.js
@@ -5,6 +5,6 @@ setInterval(() => {
   if (Number(time.innerText) > 0) {
     time.innerText = Number(time.innerText) - 1
   } else {
-    window.location.assign("http://127.0.0.1:5500/")
+    window.location.assign("https://gabrielsapucci.github.io/")
   }
 }, 1000);
